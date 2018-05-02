31e8fe261b451f2b5da2f9d8ada0942428931eeb
kapsalhs <kapsalhs1997@gmail.com>
2018-05-02 05:16:47 +0300

    Complete

---
+++ Assets/Scripts/Destroy_By_Contact.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_By_Contact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+
+	private Game_Controller gameController;
+
+	void Start(){
+
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<Game_Controller> ();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find GameController Script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+
+		gameController.AddScore (scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+
+
+	}
+}
---
+++ Assets/Scripts/Random_Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_Rotator : MonoBehaviour {
+
+	public float tumble;
+
+	void Start(){
+		GetComponent<Rigidbody> ().angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
