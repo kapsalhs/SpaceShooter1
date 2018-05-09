e2665b362f877275f3f33ea0faf2ea489280d740
kapsalhs <kapsalhs1997@gmail.com>
2018-05-09 03:35:32 +0300

    Fuel

--- Assets/Scripts/Refuel.cs
+++ Assets/Scripts/Refuel.cs
@@ -7,7 +7,10 @@ public class Refuel : MonoBehaviour {
 
 	public float giveFuel;
 	public GameObject playerExplosion;
+	public GameObject take_fuel;
 	private Slider sliderValue;
+	private GameObject remove_tank;
+	private float sec = 0.5f;
 
 	void Awake(){
 		sliderValue = GameObject.Find ("Slider").GetComponent<Slider> ();
@@ -15,9 +18,11 @@ public class Refuel : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.CompareTag ("Player")) {
-			other.gameObject.GetComponent<PlayerController> ().fuelCounter += giveFuel;
-			sliderValue.value += giveFuel;
+			other.gameObject.GetComponent<PlayerController> ().fuelCounter = 20;
+			sliderValue.value = 20;
+			Instantiate (take_fuel, other.transform.position, other.transform.rotation);
 			Destroy (gameObject);
+
 		} 
 		else if(!other.gameObject.CompareTag ("Boundary")) {
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
@@ -25,4 +30,5 @@ public class Refuel : MonoBehaviour {
 			Destroy (other.gameObject);
 		}
 	}
+
 }
